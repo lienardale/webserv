d3217f724f4a1d6b07ee77b9db732ef9e16db5f4
pierrecariou <pierrecariou@outlook.fr>
2021-04-15 15:05:19 +0200

    NGINX -100.0 (socket beginning)

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,11 +6,17 @@
 /*   By: alienard <alienard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/09 12:15:04 by dboyer            #+#    #+#             */
-/*   Updated: 2021/04/15 14:16:43 by pcariou          ###   ########.fr       */
+/*   Updated: 2021/04/15 15:02:40 by pcariou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "webserv.hpp"
+
+void	error_syscall(std::string message)
+{
+	std::cout << message << " .errno: " << errno << std::endl;
+	exit(EXIT_FAILURE);
+}
 
 void	socket_close(Socket	&sock)
 {
@@ -23,17 +29,11 @@ void	socket_read(Socket &sock)
 	int	addrlen = sizeof(sock.sockaddr);
 
 	if ((sock.connect = accept(sock.fd, (struct sockaddr*)&sock.sockaddr, (socklen_t*)&addrlen)) == -1)
-	{
-		std::cerr << "Failed to grab connection. errno: " << errno << std::endl;
-		exit(EXIT_FAILURE);
-	}
+		error_syscall("Failed to grab connection");
 
 	char	buffer[100];
 	if (read(sock.connect, buffer, 100) == -1)
-	{
-		std::cerr << "Failed to read. errno: " << errno << std::endl;
-		exit (EXIT_FAILURE);
-	}
+		error_syscall("Failed to read");
 	std::cout << "You just said: " << buffer << std::endl;
 }
 
@@ -44,15 +44,9 @@ void	socket_listen(Socket &sock)
 	sock.sockaddr.sin_port = htons(42);
 
 	if (bind(sock.fd, (struct sockaddr*)&sock.sockaddr, sizeof(sock.sockaddr)) == -1)
-	{
-		std::cerr << "Failed to bind to port 42. errno: " << errno << std::endl;
-		exit(EXIT_FAILURE);
-	}
+		error_syscall("Failed to bind to port 42");
 	if (listen(sock.fd, 10) == -1)
-	{
-		std::cerr << "Failed to listen on socket. errno: " << errno << std::endl;
-		exit(EXIT_FAILURE);
-	}
+		error_syscall("Failed to listen on socket");
 }
 
 void	socket_send(Socket &sock)
@@ -64,10 +58,7 @@ void	socket_send(Socket &sock)
 void	socket_create(Socket &sock)
 {
 	if ((sock.fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-	{
-		std::cerr << "Failed to create socket. errno: " << errno << std::endl;
-		exit(EXIT_FAILURE);
-	}
+		error_syscall("Failed to create socket");
 }
 
 int		main(void)
