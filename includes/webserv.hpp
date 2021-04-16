1e3adf91d6e07c516f2fe1d69626e791458307fe
pierrecariou <pierrecariou@outlook.fr>
2021-04-16 15:23:02 +0200

    pcariou

--- includes/webserv.hpp
+++ includes/webserv.hpp
@@ -6,7 +6,7 @@
 /*   By: dboyer <dboyer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/09 12:15:47 by dboyer            #+#    #+#             */
-/*   Updated: 2021/04/15 13:46:16 by pcariou          ###   ########.fr       */
+/*   Updated: 2021/04/16 13:38:04 by pcariou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,14 @@
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdio.h>
+#include <sstream>
+#include <iterator>
+#include <vector>
+#include <fstream>
 
 #include "Socket.hpp"
 
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: alienard <alienard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/09 12:15:04 by dboyer            #+#    #+#             */
-/*   Updated: 2021/04/15 17:39:16 by pcariou          ###   ########.fr       */
+/*   Updated: 2021/04/16 14:53:47 by pcariou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,37 +25,55 @@ void	socket_close(Socket	&sock)
 
 void	socket_send(Socket &sock)
 {
-	std::string response = "HTTP/1.0 200 OK\r\n\r\nHello";
-	send(sock.connect, response.c_str(), response.size(), 0);
+	std::string		content = "<h1>404 Not Found</h1>";
+	std::fstream	f;
+
+	if (sock.infos.size() >= 3 && sock.infos[0] == "GET")
+	{
+		if (sock.infos[1] == "/")
+			f.open("www/index.html");
+		else
+			f.open(("www" + sock.infos[1]).c_str(), std::ios::in);
+		if (f)
+		{
+			std::string	page((std::istreambuf_iterator<char>(f)), std::istreambuf_iterator<char>());
+			content = page;
+		}
+		f.close();
+	}
+	std::ostringstream	oss;
+	oss <<  "HTTP/1.1 200 OK\r\n\r\n";
+	oss << content;
+	send(sock.connect, oss.str().c_str(), oss.str().size(), 0);
 }
 
 void	socket_read(Socket &sock)
 {
 	for ( ; ; )
 	{
-		std::cout << "waiting for a connection on port 4222\n" << std::endl;
-		int	addrlen = sizeof(sock.sockaddr);
+		int		addrlen = sizeof(sock.sockaddr);
+		char	buffer[3000];
 
+		std::cout << "waiting for a connection on port 4222\n" << std::endl;
 		if ((sock.connect = accept(sock.fd, (struct sockaddr*)&sock.sockaddr, (socklen_t*)&addrlen)) == -1)
 			error_syscall("Failed to grab connection");
 
-		char	buffer[3000];
 		if (read(sock.connect, buffer, 3000) == -1)
 			error_syscall("Failed to read");
 		std::cout << "-- INFOS --\n\n" << buffer << std::endl << std::endl;
 
-		std::string response = "HTTP/1.0 200 OK\r\n\r\n<h1>NGINX<h1>";
-		write(sock.connect, response.c_str(), response.size());
-		//socket_send(sock);
+		std::istringstream	iss(buffer);
+		sock.infos	= std::vector<std::string>((std::istream_iterator<std::string>(iss)), std::istream_iterator<std::string>());
+		socket_send(sock);
 		close(sock.connect);
 	}
 }
 
 void	socket_listen(Socket &sock)
-{
-	sock.sockaddr.sin_family = AF_INET;
-	sock.sockaddr.sin_addr.s_addr = INADDR_ANY;
-	sock.sockaddr.sin_port = htons(4222);
+{ 
+	sock.sockaddr.sin_family = AF_INET;//address family IPv4
+	sock.sockaddr.sin_addr.s_addr = INADDR_ANY;//address (localhost)
+	sock.sockaddr.sin_port = htons(4222);//port
 
 	if (bind(sock.fd, (struct sockaddr*)&sock.sockaddr, sizeof(sock.sockaddr)) == -1)
 		error_syscall("Failed to bind to port 4222");
@@ -65,7 +83,8 @@ void	socket_listen(Socket &sock)
 
 void	socket_create(Socket &sock)
 {
-	if ((sock.fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+	//initialise a socket (return fd)
+	if ((sock.fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)// IPv4, binaires, TCP
 		error_syscall("Failed to create socket");
 }
 
@@ -76,7 +95,6 @@ int		main(void)
 	socket_create(sock);
 	socket_listen(sock);
 	socket_read(sock);
-	//socket_send(sock);
 	socket_close(sock);
 
 	return 0;
