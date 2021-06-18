55beb6fc5913ba48c83e65f82fc385e06723d4d3
BoyerDamien <dboyer@student.42.fr>
2021-06-18 12:00:37 +0200

    before create server branch

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,43 +6,37 @@
 /*   By: alienard <alienard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/09 12:15:04 by dboyer            #+#    #+#             */
-/*   Updated: 2021/06/17 15:54:07 by pcariou          ###   ########.fr       */
+/*   Updated: 2021/06/18 12:00:04 by dboyer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "webserv.hpp"
 
-std::list< http::Server >	SERVERS;
+http::Server SERVER;
 
-void handleSignal(int sig)
+void handleSignal( int sig )
 {
-	(void)sig;
+	( void )sig;
 
-	for (std::list< http::Server >::iterator it=SERVERS.begin(); it != SERVERS.end(); ++it)
-		it->stop();
+	SERVER.stop();
 }
 
-int main(void)
-{	
-	try {
-		config	conf;
+int main( void )
+{
+	try
+	{
+		config conf;
 
 		// Signal handling
-		std::signal(SIGINT, handleSignal);
-		std::signal(SIGTERM, handleSignal);
-		std::signal(SIGQUIT, handleSignal);
-		
-		// list dataserv
-		std::list< t_serverData >	data = conf.getContent();
+		std::signal( SIGINT, handleSignal );
+		std::signal( SIGTERM, handleSignal );
+		std::signal( SIGQUIT, handleSignal );
 
-		//create servers and push them in list (SERVERS)
-		for (std::list< t_serverData >::iterator it=data.begin(); it != data.end(); ++it)	
-			SERVERS.push_back(http::Server(it->listen, it->client_max_body_size));
-		//iter through SERVERS and listen each of them
-		for (std::list< http::Server >::iterator it=SERVERS.begin(); it != SERVERS.end(); ++it)
-			it->listen();
+		// list dataserv
+		SERVER.init( conf.getContent(), 1 );
 	}
-	catch (config::ConfigFileException const &e) {
+	catch ( config::ConfigFileException const &e )
+	{
 		std::cerr << e.what() << std::endl;
 	}
 	return 0;
--- srcs/server.cpp
+++ srcs/server.cpp
@@ -6,12 +6,15 @@
 /*   By: dess <dboyer@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/19 14:54:53 by dess              #+#    #+#             */
-/*   Updated: 2021/06/17 15:53:34 by pcariou          ###   ########.fr       */
+/*   Updated: 2021/06/18 11:57:57 by dboyer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "server.hpp"
+#include "parsing/dataStructure.hpp"
 #include "socket.hpp"
+#include <list>
+#include <utility>
 
 /******************************************************************************
  *			Constructeurs/Destructeurs
@@ -22,31 +25,30 @@
  *	@Parametres: Le port qui sera écouté et l'interval (en seconde) entre chaque écoute
  *	@Lien: http://manpagesfr.free.fr/man/man2/select.2.html
  */
-http::Server::Server(uint32_t port, uint32_t timeout) : _port(port), _run(false)
+http::Server::Server() : _run( false )
 {
-	_timeout.tv_sec = timeout;
-	_timeout.tv_usec = 0;
-	FD_ZERO(&_readSet);
-	FD_ZERO(&_writeSet);
-	FD_ZERO(&_exceptSet);
+
+	FD_ZERO( &_readSet );
+	FD_ZERO( &_writeSet );
+	FD_ZERO( &_exceptSet );
 }
 
 /*
  *	Constructeur par copie
  */
-http::Server::Server(const Server &other)
-	: _port(other._port), _timeout(other._timeout), _serverSocket(Socket(other._serverSocket)),
-	  _readSet(other._readSet), _writeSet(other._writeSet), _exceptSet(other._exceptSet), _run(other._run)
+http::Server::Server( const Server &other )
+	: _timeout( other._timeout ), _configs( other._configs ), _serverSet( other._serverSet ),
+	  _readSet( other._readSet ), _writeSet( other._writeSet ), _exceptSet( other._exceptSet ), _run( other._run )
 {
 }
 
 /*
  *	Construction par assignation
  */
-http::Server &http::Server::operator=(const Server &other)
+http::Server &http::Server::operator=( const Server &other )
 {
-	_port = other._port;
-	_serverSocket = other._serverSocket;
+	_configs = other._configs;
+	_serverSet = other._serverSet;
 	_readSet = other._readSet;
 	_writeSet = other._writeSet;
 	_exceptSet = other._exceptSet;
@@ -54,7 +56,7 @@ http::Server &http::Server::operator=(const Server &other)
 	return *this;
 }
 
-http::Server::~Server(void)
+http::Server::~Server( void )
 {
 }
 
@@ -62,22 +64,34 @@ http::Server::~Server(void)
  *			Fonctions membres
  ***************************************************************************************/
 
+void http::Server::init( const std::list< t_serverData > &configs, uint32_t timeout )
+{
+	_timeout.tv_sec = timeout;
+	_timeout.tv_usec = 0;
+	_configs = configs;
+}
+
 /*
  *	Fonction qui va mettre le serveur en écoute sur le port spécifié dans le constructeur
  *	@Infos: la fonction arrête le programme si une SocketException est levé
  */
-void http::Server::listen(void)
+void http::Server::listen( void )
 {
 	try
 	{
-		_serverSocket.listen(_port);
-		FD_SET(_serverSocket.Fd(), &_readSet);
+		for ( std::list< t_serverData >::iterator it = _configs.begin(); it != _configs.end(); it++ )
+		{
+			Socket s;
+			s.listen( it->listen );
+			_serverSet[ s.Fd() ] = std::make_pair( s, *it );
+			FD_SET( s.Fd(), &_readSet );
+		}
 		_watchFds();
 	}
-	catch (Socket::SocketException &e)
+	catch ( Socket::SocketException &e )
 	{
 		std::cerr << e.what() << std::endl;
-		exit(EXIT_FAILURE);
+		exit( EXIT_FAILURE );
 	}
 }
 
@@ -87,36 +101,37 @@ void http::Server::listen(void)
  *	@Infos La fonction lève une SocketException si erreur
  */
 
-void http::Server::_handleRead(const int fd) throw(Socket::SocketException)
+void http::Server::_handleRead( const int fd ) throw( Socket::SocketException )
 {
-	if (_serverSocket == fd)
+	std::map< int, std::pair< Socket, t_serverData > >::iterator found = _serverSet.find( fd );
+
+	if ( found != _serverSet.end() )
 	{
 		// this is a new connection
-		Socket clientSocket = _serverSocket.accept();
-		FD_SET(clientSocket.Fd(), &_readSet);
+		found->second.first.accept();
+		FD_SET( found->second.first.Fd(), &_readSet );
 	}
 	else
 	{
 		// read from the client
-		_currentSock = Socket(fd, true);
+		_currentSock = Socket( fd, true );
 		_currentSock.readContent();
 
-		FD_CLR(fd, &_readSet);
-		FD_SET(fd, &_writeSet);
+		FD_CLR( fd, &_readSet );
+		FD_SET( fd, &_writeSet );
 	}
 }
 
 /*
  *	Fonction qui se charge de la l'envoie des données au client
  *	@Parametres: Le fd sur l'écriture doit se faire
- *	@Info: La fonction est vide pour le moment. Mais c'est ici qu'il écrire sur le bon fd
  */
-void http::Server::_handleWrite(const int fd)
+void http::Server::_handleWrite( const int fd )
 {
 	// write to the client
 	_currentSock.serverResponse();
-	FD_CLR(fd, &_writeSet);
-	close(fd);
+	FD_CLR( fd, &_writeSet );
+	close( fd );
 }
 
 /*
@@ -124,31 +139,31 @@ void http::Server::_handleWrite(const int fd)
  *	@Infos: La fonction lève une SocketException si erreur
  *	@Lien: http://manpagesfr.free.fr/man/man2/select.2.html
  */
-void http::Server::_watchFds(void) throw(Socket::SocketException)
+void http::Server::_watchFds( void ) throw( Socket::SocketException )
 {
 	fd_set readyReadSet, readyWriteSet;
 
 	_run = true;
-	while (_run)
+	while ( _run )
 	{
 		// Make a copy because select is destructive !!
 		readyReadSet = _readSet;
 		readyWriteSet = _writeSet;
-		if (select(FD_SETSIZE, &readyReadSet, &readyWriteSet, NULL, &_timeout) < 0)
+		if ( select( FD_SETSIZE, &readyReadSet, &readyWriteSet, NULL, &_timeout ) < 0 )
 		{
-			if (_run)
+			if ( _run )
 			{
-				std::cerr << "Error: select " << strerror(errno) << std::endl;
+				std::cerr << "Error: select " << strerror( errno ) << std::endl;
 				stop();
 			}
-			exit(EXIT_FAILURE);
+			exit( EXIT_FAILURE );
 		}
-		for (int i = 0; i < FD_SETSIZE; i++)
+		for ( int i = 0; i < FD_SETSIZE; i++ )
 		{
-			if (FD_ISSET(i, &readyReadSet))
-				_handleRead(i);
-			if (FD_ISSET(i, &readyWriteSet))
-				_handleWrite(i);
+			if ( FD_ISSET( i, &readyReadSet ) )
+				_handleRead( i );
+			if ( FD_ISSET( i, &readyWriteSet ) )
+				_handleWrite( i );
 		}
 	}
 }
@@ -156,17 +171,17 @@ void http::Server::_watchFds(void) throw(Socket::SocketException)
 /*
  *	Fonction qui stop le serveur et ferme tous les fd ouverts
  */
-void http::Server::stop(void)
+void http::Server::stop( void )
 {
 	std::cout << "Gracefully stopping the server..." << std::endl;
-	if (_run)
+	if ( _run )
 	{
 		_run = false;
-		for (int i = 0; i < FD_SETSIZE + 1; i++)
+		for ( int i = 0; i < FD_SETSIZE + 1; i++ )
 		{
-			if (FD_ISSET(i, &_readSet) || FD_ISSET(i, &_writeSet))
+			if ( FD_ISSET( i, &_readSet ) || FD_ISSET( i, &_writeSet ) )
 			{
-				Socket(i, true).close();
+				Socket( i, true ).close();
 			}
 		}
 	}
