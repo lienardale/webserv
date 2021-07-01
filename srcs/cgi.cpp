26270c36aaefa833220387b70282d084b012a1ce
dboyer <dboyer@student.42.fr>
2021-07-01 14:55:30 +0200

    request operator << overload

--- srcs/cgi.cpp
+++ srcs/cgi.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cgi.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alienard <alienard@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dboyer <dboyer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/24 15:07:47 by akira             #+#    #+#             */
-/*   Updated: 2021/06/30 12:16:16 by alienard         ###   ########.fr       */
+/*   Updated: 2021/07/01 14:39:40 by dboyer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,34 +18,32 @@
 
 cgi::cgi()
 {
-
 }
 
-cgi::cgi( Socket &sock, t_serverData &data ) // throw( cgi::CGIException )
+cgi::cgi(Socket &sock, t_serverData &data) // throw( cgi::CGIException )
 {
-	setCgi(sock, data);
-	// if (setCgi(sock))
-	// 	throw( cgi::CGIException() );
+    setCgi(sock, data);
+    // if (setCgi(sock))
+    // 	throw( cgi::CGIException() );
 }
 
 cgi::~cgi()
 {
-
 }
 
-cgi::cgi(const cgi& obj)
+cgi::cgi(const cgi &obj)
 {
-	*this = obj;
+    *this = obj;
 }
 
 /******************************************************************************
  *			Operator overloading
  *****************************************************************************/
 
-cgi&	cgi::operator=(const cgi& obj)
+cgi &cgi::operator=(const cgi &obj)
 {
-	(void)obj;
-	return *this;
+    (void)obj;
+    return *this;
 }
 
 /******************************************************************************
@@ -63,86 +61,85 @@ cgi&	cgi::operator=(const cgi& obj)
  *							Fonction membres
  *****************************************************************************/
 
-	// std::fstream fs;
-	// fs.open("test.txt", std::fstream::out);
-	// fs << sock.get_request();
-	// fs.close();
-	// std::string request;
-	// std::string found;
-	// size_t pos;
-	// // size_t npos;
-	// size_t i;
-	// pos = 0;
-	// i = 0;
-	// request = sock.get_request();
+// std::fstream fs;
+// fs.open("test.txt", std::fstream::out);
+// fs << sock.get_request();
+// fs.close();
+// std::string request;
+// std::string found;
+// size_t pos;
+// // size_t npos;
+// size_t i;
+// pos = 0;
+// i = 0;
+// request = sock.get_request();
 
-	// while (i < LEN_CGI_ENV - 1 && (pos = request.find(" ")) != std::string::npos)
-	// {
-	// 	std::cout << "\nPOS " << i << " : " << pos << std::endl;
-	// 	// if (i == LEN_CGI_ENV)
-	// 	// 	break ;
-	// 	env[i] = const_cast<char*>(request.substr(0, pos).c_str());
-	// 	request.erase(0, pos + 1);
-	// 	std::cout << i << " | " << env[i] << std::endl;
-	// 	i++;
-	// }
-	// env[LEN_CGI_ENV] = NULL;
+// while (i < LEN_CGI_ENV - 1 && (pos = request.find(" ")) != std::string::npos)
+// {
+// 	std::cout << "\nPOS " << i << " : " << pos << std::endl;
+// 	// if (i == LEN_CGI_ENV)
+// 	// 	break ;
+// 	env[i] = const_cast<char*>(request.substr(0, pos).c_str());
+// 	request.erase(0, pos + 1);
+// 	std::cout << i << " | " << env[i] << std::endl;
+// 	i++;
+// }
+// env[LEN_CGI_ENV] = NULL;
 
-
-void cgi::setCgiMetaVar( Socket &sock, t_serverData &data)
+void cgi::setCgiMetaVar(Socket &sock, t_serverData &data)
 {
-	(void) data;
-	s_env._auth_type = "AUTH_TYPE=" + sock.get_m_request()->header( "AuthType" );
-	s_env._content_length = "CONTENT_LENGTH=" + sock.get_m_request()->header( "Content-Length" );
-	s_env._content_type = "CONTENT_TYPE=" + sock.get_m_request()->header( "Content-Type" );
-	s_env._path_info = "PATH_INFO=" + sock.get_infos()[1];
-	s_env._path_translated = "PATH_TRANSLATED=" + sock.get_m_request()->header( "      " );
-	s_env._query_string = "QUERY_STRING=" + sock.get_m_request()->uri(); // to parse
-	s_env._remote_addr = "REMOTE_ADDR=" + sock.get_m_request()->header( "              " );
-	s_env._remote_host = "REMOTE_HOST=" + sock.get_m_request()->host();
-	s_env._remote_ident = "REMOTE_IDENT=" + sock.get_m_request()->header( "            " );
-	s_env._remote_user = "REMOTE_USER=" + sock.get_m_request()->header( "              " );
-	s_env._request_method = "REQUEST_METHOD=" + sock.get_m_request()->method();
-	s_env._script_name = "SCRIPT_NAME=" + sock.get_m_request()->header( "              " );
-	s_env._server_port = "SERVER_PORT=" + sock.get_m_request()->header( "              " );
-	s_env._server_protocol = "SERVER_PROTOCOL=" + sock.get_m_request()->protocol();
-	s_env._gateway_interface = "GATEWAY_INTERFACE=" + sock.get_m_request()->header( "AuthType" );
-	s_env._server_name = "SERVER_NAME=" + sock.get_m_request()->header( "              " );
-	s_env._server_software = "SERVER_SOFTWARE=" + sock.get_m_request()->header( "      " );
-	s_env._http_accept = "HTTP_ACCEPT=" + sock.get_m_request()->header( "Accept" );
-	s_env._http_accept_language = "HTTP_ACCEPT_LANGUAGE=" + sock.get_m_request()->header( "Accept-Language" );
-	s_env._http_user_agent = "HTTP_USER_AGENT=" + sock.get_m_request()->header( "User-Agent" );
-	s_env._http_cookie = "HTTP_COOKIE=" + sock.get_m_request()->header( "AuthType" );
+    (void)data;
+    s_env._auth_type = "AUTH_TYPE=" + sock.get_m_request()->header("AuthType");
+    s_env._content_length = "CONTENT_LENGTH=" + sock.get_m_request()->header("Content-Length");
+    s_env._content_type = "CONTENT_TYPE=" + sock.get_m_request()->header("Content-Type");
+    s_env._path_info = "PATH_INFO=" + sock.get_infos()[1];
+    s_env._path_translated = "PATH_TRANSLATED=" + sock.get_m_request()->header("      ");
+    s_env._query_string = "QUERY_STRING=" + sock.get_m_request()->uri(); // to parse
+    s_env._remote_addr = "REMOTE_ADDR=" + sock.get_m_request()->header("              ");
+    s_env._remote_host = "REMOTE_HOST=" + sock.get_m_request()->header("Host");
+    s_env._remote_ident = "REMOTE_IDENT=" + sock.get_m_request()->header("            ");
+    s_env._remote_user = "REMOTE_USER=" + sock.get_m_request()->header("              ");
+    s_env._request_method = "REQUEST_METHOD=" + sock.get_m_request()->method();
+    s_env._script_name = "SCRIPT_NAME=" + sock.get_m_request()->header("              ");
+    s_env._server_port = "SERVER_PORT=" + sock.get_m_request()->header("              ");
+    s_env._server_protocol = "SERVER_PROTOCOL=" + sock.get_m_request()->protocol();
+    s_env._gateway_interface = "GATEWAY_INTERFACE=" + sock.get_m_request()->header("AuthType");
+    s_env._server_name = "SERVER_NAME=" + sock.get_m_request()->header("              ");
+    s_env._server_software = "SERVER_SOFTWARE=" + sock.get_m_request()->header("      ");
+    s_env._http_accept = "HTTP_ACCEPT=" + sock.get_m_request()->header("Accept");
+    s_env._http_accept_language = "HTTP_ACCEPT_LANGUAGE=" + sock.get_m_request()->header("Accept-Language");
+    s_env._http_user_agent = "HTTP_USER_AGENT=" + sock.get_m_request()->header("User-Agent");
+    s_env._http_cookie = "HTTP_COOKIE=" + sock.get_m_request()->header("AuthType");
 }
 
-void cgi::setCgiEnv( void )
+void cgi::setCgiEnv(void)
 {
-	env[AUTH_TYPE] = const_cast<char*>(s_env._auth_type.c_str());
-	env[CONTENT_LENGTH] = const_cast<char*>(s_env._content_length.c_str());
-	env[CONTENT_TYPE] = const_cast<char*>(s_env._content_type.c_str());
-	env[PATH_INFO] = const_cast<char*>(s_env._path_info.c_str());
-	env[PATH_TRANSLATED] = const_cast<char*>(s_env._path_translated.c_str());
-	env[QUERY_STRING] = const_cast<char*>(s_env._query_string.c_str());
-	env[REMOTE_ADDR] = const_cast<char*>(s_env._remote_addr.c_str());
-	env[REMOTE_HOST] = const_cast<char*>(s_env._remote_host.c_str());
-	env[REMOTE_IDENT] = const_cast<char*>(s_env._remote_ident.c_str());
-	env[REMOTE_USER] = const_cast<char*>(s_env._remote_user.c_str());
-	env[REQUEST_METHOD] = const_cast<char*>(s_env._request_method.c_str());
-	env[SCRIPT_NAME] = const_cast<char*>(s_env._script_name.c_str());
-	env[SERVER_PORT] = const_cast<char*>(s_env._server_port.c_str());
-	env[SERVER_PROTOCOL] = const_cast<char*>(s_env._server_protocol.c_str());
-	env[GATEWAY_INTERFACE] = const_cast<char*>(s_env._gateway_interface.c_str());
-	env[SERVER_NAME] = const_cast<char*>(s_env._server_name.c_str());
-	env[SERVER_SOFTWARE] = const_cast<char*>(s_env._server_software.c_str());
-	env[HTTP_ACCEPT] = const_cast<char*>(s_env._http_accept.c_str());
-	env[HTTP_ACCEPT_LANGUAGE] = const_cast<char*>(s_env._http_accept_language.c_str());
-	env[HTTP_USER_AGENT] = const_cast<char*>(s_env._http_user_agent.c_str());
-	env[HTTP_COOKIE] = const_cast<char*>(s_env._http_cookie.c_str());
-	env[LEN_CGI_ENV] = NULL;
+    env[AUTH_TYPE] = const_cast<char *>(s_env._auth_type.c_str());
+    env[CONTENT_LENGTH] = const_cast<char *>(s_env._content_length.c_str());
+    env[CONTENT_TYPE] = const_cast<char *>(s_env._content_type.c_str());
+    env[PATH_INFO] = const_cast<char *>(s_env._path_info.c_str());
+    env[PATH_TRANSLATED] = const_cast<char *>(s_env._path_translated.c_str());
+    env[QUERY_STRING] = const_cast<char *>(s_env._query_string.c_str());
+    env[REMOTE_ADDR] = const_cast<char *>(s_env._remote_addr.c_str());
+    env[REMOTE_HOST] = const_cast<char *>(s_env._remote_host.c_str());
+    env[REMOTE_IDENT] = const_cast<char *>(s_env._remote_ident.c_str());
+    env[REMOTE_USER] = const_cast<char *>(s_env._remote_user.c_str());
+    env[REQUEST_METHOD] = const_cast<char *>(s_env._request_method.c_str());
+    env[SCRIPT_NAME] = const_cast<char *>(s_env._script_name.c_str());
+    env[SERVER_PORT] = const_cast<char *>(s_env._server_port.c_str());
+    env[SERVER_PROTOCOL] = const_cast<char *>(s_env._server_protocol.c_str());
+    env[GATEWAY_INTERFACE] = const_cast<char *>(s_env._gateway_interface.c_str());
+    env[SERVER_NAME] = const_cast<char *>(s_env._server_name.c_str());
+    env[SERVER_SOFTWARE] = const_cast<char *>(s_env._server_software.c_str());
+    env[HTTP_ACCEPT] = const_cast<char *>(s_env._http_accept.c_str());
+    env[HTTP_ACCEPT_LANGUAGE] = const_cast<char *>(s_env._http_accept_language.c_str());
+    env[HTTP_USER_AGENT] = const_cast<char *>(s_env._http_user_agent.c_str());
+    env[HTTP_COOKIE] = const_cast<char *>(s_env._http_cookie.c_str());
+    env[LEN_CGI_ENV] = NULL;
 }
 
-void cgi::setCgi( Socket &sock, t_serverData &data)
+void cgi::setCgi(Socket &sock, t_serverData &data)
 {
-	setCgiMetaVar( sock, data );
-	setCgiEnv();
+    setCgiMetaVar(sock, data);
+    setCgiEnv();
 }
--- srcs/request.cpp
+++ srcs/request.cpp
@@ -6,7 +6,7 @@
 /*   By: dboyer <dboyer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/29 18:29:23 by dboyer            #+#    #+#             */
-/*   Updated: 2021/07/01 13:05:28 by dboyer           ###   ########.fr       */
+/*   Updated: 2021/07/01 14:49:31 by dboyer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,131 +18,155 @@
 #include <iterator>
 #include <list>
 #include <map>
+#include <ostream>
 #include <string>
 #include <sys/types.h>
 
 /******************************************************************************
  *				Outils
  *****************************************************************************/
-/*static std::string ft_str_trim( std::string str, char c )
+
+static std::list<std::string> ft_split(std::string str, std::string toFind)
 {
-        size_t first = str.find_first_not_of( c );
-        size_t last = str.find_last_not_of( c );
-        if ( first != std::string::npos && last != std::string::npos )
-                return str.substr( first, ( last - first + 1 ) );
-        return "";
-}*/
+    std::list<std::string> l;
+    std::string::size_type pos;
+    std::string s;
 
-static std::list<std::string> ft_split(std::string str, std::string toFind) {
-  std::list<std::string> l;
-  std::string::size_type pos;
-  std::string s;
-
-  while ((pos = str.find(toFind)) != std::string::npos &&
-         str.begin() != str.end()) {
-    l.push_back(str.substr(0, pos));
-    str.erase(str.begin(), str.begin() + pos + 1);
-  }
-  if (pos == std::string::npos)
-    l.push_back(str.substr(0, pos));
-  return l;
+    while ((pos = str.find(toFind)) != std::string::npos && str.begin() != str.end())
+    {
+        l.push_back(str.substr(0, pos));
+        str.erase(str.begin(), str.begin() + pos + 1);
+    }
+    if (pos == std::string::npos)
+        l.push_back(str.substr(0, pos));
+    return l;
 }
-/*
 
- static void ft_clean( std::string &str, std::string toRemove )
- {
-         std::string::size_type pos;
-         while ( ( pos = str.find( toRemove ) ) != std::string::npos )
-                 str.erase( str.begin() + pos );
- }*/
+static void extractHeader(std::map<std::string, std::string> &headers, std::string content) throw(ParsingException)
+{
 
-static void extractHeader(std::map<std::string, std::string> &headers,
-                          std::string content) throw(ParsingException) {
+    std::string key, value;
+    std::string::size_type pos;
 
-  std::string key, value;
-  std::string::size_type pos;
+    pos = content.find(": ");
+    if (pos != std::string::npos)
+    {
+        key = content.substr(0, pos);
+        value = content.substr(pos + 2, &*content.rend() - &content[pos + 2]);
 
-  pos = content.find(": ");
-  if (pos != std::string::npos) {
-    key = content.substr(0, pos);
-    value = content.substr(pos + 2, &*content.rend() - &content[pos + 2]);
-
-    if (headers.find(key) != headers.end())
-      headers[key] += "," + value;
+        if (headers.find(key) != headers.end())
+            headers[key] += "," + value;
+        else
+            headers[key] = value;
+    }
     else
-      headers[key] = value;
-  } else
-    throw BadRequest("Wrong key value syntax");
+        throw BadRequest("Wrong key value syntax");
 }
 
 /******************************************************************************
  *               Constructeurs
  ******************************************************************************/
-Request::Request(std::string content) throw(ParsingException) {
-  int n = 0;
-  bool body = false;
-  std::string::size_type pos;
+Request::Request(std::string content) throw(ParsingException)
+{
+    int n = 0;
+    bool body = false;
+    std::string::size_type pos;
 
-  while ((pos = content.find("\r\n")) != std::string::npos) {
-    std::string s = content.substr(0, pos);
-    content.erase(content.begin(), content.begin() + pos + 2);
+    while ((pos = content.find("\r\n")) != std::string::npos)
+    {
+        std::string s = content.substr(0, pos);
+        content.erase(content.begin(), content.begin() + pos + 2);
 
-    if (n == 0) {
-      std::list<std::string> query = ft_split(s, " ");
-      if (query.size() != 3)
-        throw BadRequest("Bad request");
-      _method = *query.begin();
-      _uri = *++query.begin();
-      _protocol = *query.rbegin();
-    } else {
-      if (s.size() > 2 && !body)
-        extractHeader(_headers, s);
-      else if (s.size() <= 2 && !body)
-        body = true;
-      else
-        _headers["Body"] += s;
+        if (n == 0)
+        {
+            std::list<std::string> query = ft_split(s, " ");
+            if (query.size() != 3)
+                throw BadRequest("Bad request");
+            _method = *query.begin();
+            _uri = *++query.begin();
+            _protocol = *query.rbegin();
+        }
+        else
+        {
+            if (s.size() > 2 && !body)
+                extractHeader(_headers, s);
+            else if (s.size() <= 2 && !body)
+                body = true;
+            else
+                _headers["Body"] += s;
+        }
+        n++;
     }
-    n++;
-  }
-  if (body && content.size())
-    _headers["Body"] += content;
-  for (std::map<std::string, std::string>::iterator it = _headers.begin();
-       it != _headers.end(); it++)
-    std::cout << "Key: " << it->first << " Value: " << it->second << std::endl;
+    if (body && content.size())
+        _headers["Body"] += content;
 }
 
 Request::Request(const Request &other)
-    : _method(other._method), _uri(other._uri), _protocol(other._protocol),
-      _host(other._host), _headers(other._headers) {}
-
-Request &Request::operator=(const Request &other) {
-  (void)other;
-
-  _headers = other._headers;
-  _host = other._host;
-  _method = other._method;
-  _protocol = other._protocol;
-  _uri = other._uri;
-  return *this;
+    : _method(other._method), _uri(other._uri), _protocol(other._protocol), _host(other._host), _headers(other._headers)
+{
 }
 
-Request::~Request(void) {}
+Request &Request::operator=(const Request &other)
+{
+    (void)other;
 
+    _headers = other._headers;
+    _host = other._host;
+    _method = other._method;
+    _protocol = other._protocol;
+    _uri = other._uri;
+    return *this;
+}
+
+Request::~Request(void)
+{
+}
+
+/******************************************************************************
+ *               Getters
+ ******************************************************************************/
+
+std::string Request::method(void) const
+{
+    return _method;
+}
+
+std::string Request::uri(void) const
+{
+    return _uri;
+}
+
+std::string Request::protocol(void) const
+{
+    return _protocol;
+}
+
+std::map<std::string, std::string> Request::getHeader() const
+{
+    return _headers;
+}
 /******************************************************************************
  *               Fonctions membres
  ******************************************************************************/
+std::string Request::header(const std::string key) const
+{
+    std::map<std::string, std::string>::const_iterator found = _headers.find(key);
+    if (found != _headers.end())
+        return found->second;
+    return "";
+}
 
-std::string Request::method(void) const { return _method; }
+/********************************************************************************
+ *					Operator overloading
+ *******************************************************************************/
+std::ostream &operator<<(std::ostream &os, const Request &r)
+{
+    os << "Method: " << r.method() << std::endl;
+    os << "Uri: " << r.uri() << std::endl;
+    os << "Protocol: " << r.protocol() << std::endl;
 
-std::string Request::uri(void) const { return _uri; }
-
-std::string Request::protocol(void) const { return _protocol; }
-
-std::string Request::host(void) const { return _host; }
-
-std::string Request::header(const std::string key) const {
-  std::map<std::string, std::string>::const_iterator found = _headers.find(key);
-  if (found != _headers.end())
-    return found->second;
-  return "";
+    std::map<std::string, std::string> headers = r.getHeader();
+    for (std::map<std::string, std::string>::iterator it = headers.begin(); it != headers.end(); it++)
+        os << it->first << ": " << it->second << std::endl;
+    return os;
 }
